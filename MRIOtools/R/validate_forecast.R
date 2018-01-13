26d95021649f0fae61fbb7bd91891d9a396c7e67
Unknown <s.pedro@gmx.de>
2018-01-13 12:04:03 +0100

    progress bar added

--- MRIOtools/R/validate_forecast.R
+++ MRIOtools/R/validate_forecast.R
@@ -9,7 +9,7 @@
 #' @export
 
 validate_forecast <- function(mat.list, years.obs, year.split, parallel = F, n.cores = 1,
-                              type = "glm", scale = F){
+                              type = "glm", scale = F, progress.bar = F){
   n.col <- ncol(mat.list[[1]])
   n.row <- nrow(mat.list[[1]])
   # create timeseries
@@ -17,13 +17,24 @@ validate_forecast <- function(mat.list, years.obs, year.split, parallel = F, n.c
   if(scale){ # scale each timeseries (center to 0, scaled by root-mean-squatered). It is necessary to transpose ts first, then scale and then transpose again, because data.tables can (as far as I know) only apply functions column-wise. Time series with only Zeros (0) are scaled to NaN and thus need to be set to 0 again. 
     ts <- (transpose(transpose(ts)[ , lapply(.SD, scale)]))[,lapply(.SD, function(x) ifelse(is.nan(x), 0, x))]
   }
-  if(parallel == T){ # parallele compution on multiple cores
-    # Initiate cluster
-    cl <- parallel::makeCluster(n.cores, type = "FORK")
-    preds_dt <- rbindlist(parLapply(cl, 1:nrow(ts), function(x) cv(as.numeric(ts[x,]), years.obs = years.obs, year.split = year.split, type = type)), idcol = T)
-    stopCluster(cl)
-  }else{ # computation only on one single core
-    preds_dt <-  rbindlist(lapply(1:nrow(ts), function(x) cv(as.numeric(ts[x,]), years.obs = years.obs, year.split = year.split, type = type)), idcol = T)
+  if(!progress.bar){
+    if(parallel == T){ # parallele compution on multiple cores
+      # Initiate cluster
+      cl <- parallel::makeCluster(n.cores, type = "FORK")
+      preds_dt <- rbindlist(parLapply(cl, 1:nrow(ts), function(x) cv(as.numeric(ts[x,]), years.obs = years.obs, year.split = year.split, type = type)), idcol = T)
+      stopCluster(cl)
+    }else{ # computation only on one single core
+      preds_dt <-  rbindlist(lapply(1:nrow(ts), function(x) cv(as.numeric(ts[x,]), years.obs = years.obs, year.split = year.split, type = type)), idcol = T)
+    }
+  }else{ # show progress bar
+    if(parallel == T){ # parallele compution on multiple cores
+      # Initiate cluster
+      cl <- parallel::makeCluster(n.cores, type = "FORK")
+      preds_dt <- rbindlist(pblapply(1:nrow(ts), function(x) cv(as.numeric(ts[x,]), years.obs = years.obs, year.split = year.split, type = type), cl = cl), idcol = T)
+      stopCluster(cl)
+    }else{ # computation only on one single core
+      preds_dt <-  rbindlist(pblapply(1:nrow(ts), function(x) cv(as.numeric(ts[x,]), years.obs = years.obs, year.split = year.split, type = type)), idcol = T)
+    }
   }
   
   return(preds_dt)
